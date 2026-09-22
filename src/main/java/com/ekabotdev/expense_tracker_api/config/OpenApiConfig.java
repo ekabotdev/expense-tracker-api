d9b23e35dc5a8ef98719fb432f0a60c8cc2bebf7
ekabotdev <ekabotdev@gmail.com>
2026-09-22 12:01:40 +0300

    Configure OpenAPI Server URL

---
+++ src/main/java/com/ekabotdev/expense_tracker_api/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package com.ekabotdev.expense_tracker_api.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Expense Tracker API")
+                        .version("1.0")
+                        .description("REST API for managing expenses"))
+                .addServersItem(
+                        new Server()
+                                .url("/")
+                                .description("Current server")
+                );
+    }
+}
